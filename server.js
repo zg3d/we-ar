53725fdb579d9a7b2ae41037942eb66da801823f
Paul Sin <jiyoungsin97@gmail.com>
2020-03-07 14:55:05 -0500

    created a landing page for traffic

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 
 
-app.use(express.static('public'));
+app.use(express.static('assets'));
 
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
