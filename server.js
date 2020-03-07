b7cef00d2e0be6ed33e13a88a41e051e0008aff5
zg3d <devzs6.98@gmail.com>
2020-03-07 13:32:31 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,29 @@
 require('dotenv').config();
 const express = require("express");
 const handlebars = require("express-handlebars");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+
+app.use(express.static('public'));
+
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+
+
+
+
+app.get('/', function (req, res) {
+    res.render('home',{
+        title: "Home",
+        pageheading: "Homepage",
+      
+        
+    });
+});
+
+
+
+app.listen(PORT,()=>console.log("Web server has started"));
