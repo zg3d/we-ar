7850d0dbb390068cb5fd8fd48a3983c963882100
Niaz Ul Haque <theniaz619.2@gmail.com>
2020-03-07 15:54:15 -0500

    Niaz - Made a Login and Signup route/form template, Edideted the header (changed the names and added routing links

--- server.js
+++ server.js
@@ -12,8 +12,23 @@ app.use(express.static('public'));
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
 
+app.get('/login', function (req, res) {
+    res.render('login',{
+        title: "Login",
+        pageheading: "Login",
+      
+        
+    });
+});
 
-
+app.get('/signup', function (req, res) {
+    res.render('signup',{
+        title: "Registration",
+        pageheading: "Registration",
+      
+        
+    });
+});
 
 app.get('/', function (req, res) {
     res.render('home',{
